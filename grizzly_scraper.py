dfec97cd7fc94b829be7087fe6e25d58c78ec8aa
dhextras <mohammedsafki27@gmail.com>
2024-12-08 11:13:01 +0530

    feat: Update grizzly implemenation with cloudflare bypasser

--- grizzly_scraper.py
+++ grizzly_scraper.py
@@ -4,7 +4,6 @@ import json
 import os
 import re
 import sys
-import time
 from datetime import datetime
 from typing import Any, Dict, Optional
 
@@ -12,9 +11,8 @@ import aiohttp
 import pytz
 from dotenv import load_dotenv
 from pdfminer.high_level import extract_text
-from selenium.webdriver.chrome.options import Options
-from seleniumrequests import Chrome
 
+from utils.bypass_cloudflare import bypasser
 from utils.logger import log_message
 from utils.telegram_sender import send_telegram_message
 from utils.time_utils import get_next_market_times, sleep_until_market_open
@@ -24,7 +22,7 @@ load_dotenv()
 
 # Constants
 JSON_URL = "https://grizzlyreports.com/wp-json/wp/v2/media"
-CHECK_INTERVAL = 1  # seconds
+CHECK_INTERVAL = 3  # seconds
 PROCESSED_URLS_FILE = "data/grizzly_processed_urls.json"
 SESSION_FILE = "data/grizzly_session.json"
 TELEGRAM_BOT_TOKEN = os.getenv("GRIZZLY_TELEGRAM_BOT_TOKEN")
@@ -34,89 +32,42 @@ WS_SERVER_URL = os.getenv("WS_SERVER_URL")
 os.makedirs("data", exist_ok=True)
 
 
-def get_browser_session(driver):
-    """Get browser session data (cookies and localStorage)"""
+def load_cookies(frash=False) -> Optional[Dict[str, Any]]:
     try:
-        cookies = driver.get_cookies()
-        return {
-            "cookies": cookies,
-        }
-    except Exception as e:
-        log_message(f"Error getting browser session: {e}", "ERROR")
-        return None
+        cookies = None
+        if frash == False:
+            if not os.path.exists(SESSION_FILE):
+                log_message(f"Session file not found: {SESSION_FILE}", "WARNING")
+                return None
 
-
-async def setup_chrome_driver(headless=True, cookies=None):
-    options = Options()
-
-    if headless:
-        options.add_argument("--headless")
-    options.add_argument("--disable-gpu")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--maximize-window")
-    options.add_argument("--disable-search-engine-choice-screen")
-    options.add_argument("--disable-extensions")
-    options.add_argument("--disable-popup-blocking")
-    options.add_argument("--disable-blink-features=AutomationControlled")
-    options.add_experimental_option("excludeSwitches", ["enable-automation"])
-
-    driver = Chrome(options=options)
-
-    if cookies:
-        driver.get(JSON_URL)
-        await asyncio.sleep(1)
-        for cookie in cookies:
-            driver.add_cookie(cookie)
-
-    return driver
-
-
-async def get_new_session():
-    """Initialize browser and get new session cookies"""
-    driver = await setup_chrome_driver(headless=False)
-
-    try:
-
-        driver.get(JSON_URL)
-        while True:
-            cookies = driver.get_cookies()
-            if any(
-                cookie["name"] == "cf_clearance" and cookie["value"]
-                for cookie in cookies
-            ):
-                break
-            time.sleep(5)
-
-        session_data = get_browser_session(driver)
-        if session_data:
-            save_session_credentials(session_data)
-            return session_data
-
-    except Exception as e:
-        log_message(f"Error getting new session: {e}", "ERROR")
-    finally:
-        driver.quit()
-    return None
-
-
-def load_session_credentials() -> Optional[Dict[str, Any]]:
-    try:
-        if os.path.exists(SESSION_FILE):
             with open(SESSION_FILE, "r") as f:
-                session = json.load(f)
-                return session
+                cookies = json.load(f)
+
+        # Validate cookies again
+        if not cookies or cookies.get("cf_clearance", "") == "":
+            log_message(
+                "Invalid or missing 'cf_clearance' in cookies. Attempting to regenerate.",
+                "WARNING",
+            )
+            bypass = bypasser(JSON_URL, SESSION_FILE)
+
+            if not bypass or bypass == False:
+                return
+
+            with open(SESSION_FILE, "r") as f:
+                cookies = json.load(f)
+
+            if not cookies or cookies.get("cf_clearance", "") == "":
+                return None
+
+        return cookies
+
+    except json.JSONDecodeError:
+        log_message("Failed to decode JSON from session file.", "ERROR")
     except Exception as e:
         log_message(f"Error loading session: {e}", "ERROR")
+
     return None
-
-
-def save_session_credentials(session: Dict[str, Any]):
-    try:
-        with open(SESSION_FILE, "w") as f:
-            json.dump(session, f)
-    except Exception as e:
-        log_message(f"Error saving session: {e}", "ERROR")
 
 
 def load_processed_urls():
@@ -135,24 +86,45 @@ def save_processed_urls(urls):
 
 async def fetch_json(session, cookies):
     try:
-        driver = await setup_chrome_driver(headless=False, cookies=cookies)
-        driver.get(JSON_URL)
+        headers = {
+            "User-Agent": f"{cookies['user_agent']}",
+            "Cache-Control": "max-age=0",
+            "Cookie": f"cf_clearance:{cookies['cf_clearance']}",
+        }
 
-        page_source = driver.page_source
-        print(page_source)
-        sys.exit(1)
-        data = json.loads(page_source)
-
-        log_message(f"Fetched {len(data)} posts from JSON", "INFO")
-        return data
+        async with session.get(JSON_URL, headers=headers, cookies=cookies) as response:
+            if response.status == 200:
+                data = await response.json()
+                log_message(f"Fetched {len(data)} posts from JSON", "INFO")
+                return data
+            elif response.status == 403:
+                log_message(
+                    "CF_CLEARANCE expired trying to revalidate it while fetching json",
+                    "ERROR",
+                )
+                cookies = load_cookies(frash=True)
+                if not cookies:
+                    raise Exception("CF_CLEARANCE Failed: Post")
+                return []
+            else:
+                log_message(f"Failed to fetch JSON: HTTP {response.status}", "ERROR")
+                return []
     except Exception as e:
+        if "CF_CLEARANCE Failed" in str(e):
+            raise
         log_message(f"Error fetching JSON: {e}", "ERROR")
         return []
 
 
 async def extract_ticker_from_pdf(session, url, cookies):
     try:
-        async with session.get(url, cookies=cookies) as response:
+        headers = {
+            "User-Agent": f"{cookies['user_agent']}",
+            "Cache-Control": "max-age=0",
+            "Cookie": f"cf_clearance:{cookies['cf_clearance']}",
+        }
+
+        async with session.get(url, headers=headers, cookies=cookies) as response:
             if response.status == 200:
                 pdf_content = await response.read()
                 pdf_file = io.BytesIO(pdf_content)
@@ -170,6 +142,15 @@ async def extract_ticker_from_pdf(session, url, cookies):
                             return match
 
                 log_message(f"No ticker found in PDF: {url}", "WARNING")
+                return None
+            elif response.status == 403:
+                log_message(
+                    f"CF_CLEARANCE expired trying to revalidate it while fetching url: {url}",
+                    "ERROR",
+                )
+                cookies = load_cookies(frash=True)
+                if not cookies:
+                    raise Exception(f"CF_CLEARANCE Failed, url: {url}")
                 return None
             else:
                 log_message(f"Failed to fetch PDF: HTTP {response.status}", "ERROR")
@@ -214,15 +195,11 @@ async def send_to_telegram(url, ticker):
 
 async def run_scraper():
     processed_urls = load_processed_urls()
-    session_data = load_session_credentials()
+    cookies = load_cookies()
 
-    if not session_data:
-        session_data = await get_new_session()
-        if not session_data:
-            log_message("Failed to get initial session", "CRITICAL")
-            return
-
-    cookies = session_data["cookies"]
+    if not cookies:
+        log_message("Failed to get valid cf_clearance", "CRITICAL")
+        return
 
     async with aiohttp.ClientSession() as session:
         while True:
@@ -254,7 +231,6 @@ async def run_scraper():
 
                 if new_urls:
                     log_message(f"Found {len(new_urls)} new posts to process.", "INFO")
-                    await send_posts_to_telegram(new_urls)
 
                     for url in new_urls:
                         if url.lower().endswith(".pdf"):
@@ -264,6 +240,8 @@ async def run_scraper():
                             if ticker:
                                 await send_to_telegram(url, ticker)
                         processed_urls.add(url)
+
+                    await send_posts_to_telegram(new_urls)
                     save_processed_urls(processed_urls)
                 else:
                     log_message("No new posts found.", "INFO")
