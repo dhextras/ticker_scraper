9ff3cebed223078ca95b06ae4ba9e1b5777c216d
dhextras <mohammedsafki27@gmail.com>
2025-02-04 16:33:19 +0530

    chore: increase timeout

--- bearcave_xml_scraper.py
+++ bearcave_xml_scraper.py
@@ -101,7 +101,7 @@ async def fetch_xml_feed(session, raw_proxy=None):
             f"{XML_FEED_URL}?{random_cache_buster}",
             headers=headers,
             proxy=proxy,
-            timeout=2,
+            timeout=5,
         ) as response:
             if response.status == 200:
                 content = await response.text()
